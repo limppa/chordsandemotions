5a541a2ba80f17437223cd5e97ab2744ee6f0dd9
limppa <64261979+limppa@users.noreply.github.com>
2020-11-22 18:09:54 +0100

    Add files via upload

--- cae classes.py
+++ cae classes.py
@@ -1,28 +1,32 @@
 
-class Chord:
+class Chord:    #Maj7, min7, Dom7, min7b5
 
     def __init__(self,name):
         self.name = name
-        #self.emo = None
         
-    class Emotion:
+    class Emotion:  #angr, disg, fear, hap, sad, surp
 
-        def __init__(self,name,score):
+        def __init__(self,name,score=0):
             self.name = name
             self.score = score
 
-        class Compared:
+        class Compared: #has it been compared to another emotion and what was the result
 
-            def __init__(self,name):
+            def __init__(self,name,result=0):
                 self.name = name
+                self.result = result
                 
 
 Maj7 = Chord('Maj7')
 
-Maj7angr = Maj7.Emotion('angr',5)
+Maj7angr = Maj7.Emotion('angr')
+
+Maj7angrdisg = Maj7angr.Compared('disg', 1) #Maj7 chord played, angr vs disg -> angr won
 
 
-print(Maj7angr.score)
+print(Maj7angr.score)   #how well does angr match the Maj7 chord?
+
+print(Maj7angrdisg.result)  #who won, angr vs disg, when Maj7 was played?
 
 
 
